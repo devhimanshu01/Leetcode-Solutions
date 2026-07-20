5abecae8d1eceb21fd11a0f11a427bcfe664274d
Himanshu Kushwaha <himanshukushwaha.dev@gmail.com>
2026-07-20 21:01:28 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (92.73%) - LeetSync

---
+++ 4290-valid-elements-in-an-array/valid-elements-in-an-array.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findValidElements(int[] nums) {
+        int n = nums.length;
+        List<Integer> result = new ArrayList<>();
+
+        if (n == 1) {
+            result.add(nums[0]);
+            return result;
+        }
+
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+
+        leftMax[0] = Integer.MIN_VALUE;
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], nums[i - 1]);
+        }
+
+        rightMax[n - 1] = Integer.MIN_VALUE;
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], nums[i + 1]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > leftMax[i] || nums[i] > rightMax[i]) {
+                result.add(nums[i]);
+            }
+        }
+
+        return result;
+    }
+}
